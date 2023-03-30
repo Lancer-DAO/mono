cebb489725146cf13ca0e57708ff6c59511347ea
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2023-03-30 16:12:27 +0530

    add: tailwind

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -3,6 +3,7 @@ import "@/src/styles/app.scss";
 import "@/src/styles/Form.scss";
 import "@/src/styles/Bounty.scss";
 import "@/src/styles/webflow.scss";
+import "@/styles/tailwind.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
