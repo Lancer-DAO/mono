a1f607c2c8906d4962344dc4934e201835266640
jacksturt <117492794+jacksturt@users.noreply.github.com>
2023-04-06 12:50:50 -0600

    test2
    testing again

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -3,5 +3,5 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  return <div>Hey There Test</div>;
+  return <div>Hey There Test2</div>;
 }
