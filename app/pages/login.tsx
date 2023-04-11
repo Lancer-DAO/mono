a3cfd5c3ea3d76fadf4f9ec721d2e5f3bd2ad8b7
jacksturt <jack@sturt.io>
2023-04-10 19:51:36 -0600

    f

--- app/pages/login.tsx
+++ app/pages/login.tsx
@@ -63,7 +63,7 @@ const Login = () => {
   const github = async () => {
     await magic?.oauth.loginWithRedirect({
       provider: "github",
-      redirectURI: `http://localhost:8080/login`,
+      redirectURI: `https://app-kbmqmgviw-lancer-so.vercel.app/login?referrer=${referrer}`,
       scope: ["user, repo"],
     });
   };
