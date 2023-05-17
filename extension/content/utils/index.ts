19751baeefe782d2469ebabf9d96d4e03fa1e38e
jacksturt <jack@sturt.io>
2023-05-17 13:21:51 -0600

    update extension

--- extension/content/utils/index.ts
+++ extension/content/utils/index.ts
@@ -1,11 +1,11 @@
 import { PublicKey } from "@solana/web3.js";
 
-export const DEVNET_USDC_MINT = "4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU";
+export const USDC_MINT = "4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU";
 export const MAINNET_USDC_MINT = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";
 export const EXTENSION_DEV = false;
-export const APP_ENDPOINT = 'https://app-git-test-magic-link-lancer-so.vercel.app'
+export const APP_ENDPOINT = 'https://app-3rtv4mzj0-lancer-so.vercel.app/'
 export const APP_ENDPOINT_DEV = 'http://localhost:3000/'
-export const API_ENDPOINT = 'https://app-git-test-magic-link-lancer-so.vercel.app'
+export const API_ENDPOINT = 'https://app-3rtv4mzj0-lancer-so.vercel.app/'
 export const API_ENDPOINT_DEV = 'http://localhost:3000'
 export const getApiEndpointExtension = (): string => {
     return EXTENSION_DEV ? API_ENDPOINT_DEV : API_ENDPOINT;
@@ -19,7 +19,7 @@ export const getMintName = (mint?: PublicKey) => {
       return 'USDC'
     }
     const mintString = mint.toString()
-    if(mintString === DEVNET_USDC_MINT || mintString === MAINNET_USDC_MINT) {
+    if(mintString === USDC_MINT || mintString === MAINNET_USDC_MINT) {
       return 'USDC'
     }
 
