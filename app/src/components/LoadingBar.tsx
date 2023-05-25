14af383b0f19ae010630da65d4d6762940a52428
cdgrody <cdgrody@gmail.com>
2023-05-25 00:40:53 -0400

    converted LoadingBar.tsx to tailwind

--- app/src/components/LoadingBar.tsx
+++ app/src/components/LoadingBar.tsx
@@ -2,8 +2,8 @@ import ReactLoading from "react-loading";
 
 export const LoadingBar: React.FC<{ title: string }> = ({ title }) => {
   return (
-    <div className="loading-bar">
-      <div className="loading-bar-title">{title}</div>
+    <div className="flex w-full items-center justify-center gap-x-20">
+      <div className="text-14bb88 leading-28 text-28">{title}</div>
       <ReactLoading type={"bubbles"} color={"#14bb88"} height={"60px"} />
     </div>
   );
