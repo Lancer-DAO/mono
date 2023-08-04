064e0af9558d7cb91d5dd8f29308a0db8b2bb696
mattdegods <matthewcpfeifer@gmail.com>
2023-08-03 20:30:05 -0400

    Fix: indexing + organization

---
+++ app/components/account/components/BountyNFTCard.tsx
@@ -0,0 +1,35 @@
+import { BountyNFT } from "@/types/";
+import Image from "next/image";
+
+export const BountyNFTCard = ({ bountyNFT }: { bountyNFT: BountyNFT }) => {
+  return (
+    <>
+      <div className="flex items-center gap-10 border border-white-900 rounded-[20px] p-4 max-w-[600px] mx-auto">
+        <Image
+          src={"/assets/images/Lancer Gradient No Background 1.png"}
+          width={80}
+          height={80}
+          alt="Lancer Logo"
+        />
+        {/* <img src={bountyNFT.image} className="bounty-picture" /> */}
+
+        <div className="flex flex-col items-center justify-center">
+          <h4>{bountyNFT.name}</h4>
+          <div className="bounty-nft-subheader">
+            {bountyNFT.reputation} pts | {bountyNFT.completed?.fromNow()} |{" "}
+            {bountyNFT.role}
+          </div>
+        </div>
+        {bountyNFT.tags?.length > 0 && (
+          <div className="tag-list">
+            {bountyNFT.tags.map((badge) => (
+              <div className="tag-item" key={badge}>
+                {badge}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
--- app/components/bounties/Bounties/Bounties.tsx
+++ app/components/bounties/Bounties/Bounties.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import DefaultLayout from "@/components/templates/DefaultLayout";
+import { DefaultLayout } from "@/components";
 import BountyTable from "./components/BountyTable/BountyTable";
 
 export const Bounties = () => {
