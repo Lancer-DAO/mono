24899dcffb111e1ceb00a1ee700f80b151db8361
ssamkkim <ssamuel.kkim@gmail.com>
2023-08-23 21:10:57 -0700

    separated ReferenceDialogue from AddMediaForm

--- app/components/organisms/AddMediaForm.tsx
+++ app/components/organisms/AddMediaForm.tsx
@@ -1,142 +1,17 @@
-import { ImageUpload, PreviewCardBase } from "@/components";
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/atoms/Modal";
+import ReferenceDialogue from "@/components/molecules/ReferenceDialogue";
 import { smallClickAnimation } from "@/src/constants";
-import { UploadButton, UploadDropzone } from "@/src/utils/uploadthing";
 import { FORM_SECTION, FormData, Media } from "@/types/forms";
 import "@uploadthing/react/styles.css";
 import { motion } from "framer-motion";
-import { Plus, Trash, X } from "lucide-react";
+import { X } from "lucide-react";
 import Image from "next/image";
-import { Dispatch, FC, SetStateAction, useEffect, useState } from "react";
+import { Dispatch, FC, SetStateAction, useEffect } from "react";
 
 interface Props {
   setFormSection: Dispatch<SetStateAction<FORM_SECTION>>;
   formData: FormData;
   setFormData: Dispatch<SetStateAction<FormData>>;
   handleChange: (event) => void;
-}
-
-const ReferenceDialogue = ({ onReferenceAdded }) => {
-  const [reference, setReference] = useState({
-    imageUrl: "",
-    title: "",
-    description: "",
-  })
-
-  const handleImageUpload = (url) => {
-    setReference((prevReference) => ({
-      ...prevReference,
-      imageUrl: url,
-    }));
-  };
-
-  const handleTitleChange = (event) => {
-    setReference((prevReference) => ({
-      ...prevReference,
-      title: event.target.value,
-    }));
-  };
-
-  const handleDescriptionChange = (event) => {
-    setReference((prevReference) => ({
-      ...prevReference,
-      description: event.target.value,
-    }));
-  };
-
-  const handleSubmit = () => {
-    const newReference = {
-      imageUrl: reference.imageUrl,
-      title: reference.title,
-      description: reference.description,
-    };
-
-    onReferenceAdded(newReference); // Call the onReferenceAdded prop with the new reference
-    setReference({
-      imageUrl: "",
-      title: "",
-      description: "",
-    });
-  };
-
-  const handleImageDelete = () => {
-    setReference((prevReference) => ({
-      ...prevReference,
-      imageUrl: "",
-    }));
-  };
-
-  return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <button className="flex justify-center items-center border border-gray-300 rounded-md h-44 hover:bg-gray-200">
-          <Plus size={48} />
-        </button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Add a Reference</DialogTitle>
-          <DialogDescription>
-            Make changes to your reference here. Click save when you're done.
-          </DialogDescription>
-        </DialogHeader>
-        
-        <div className="flex flex-col gap-4 py-4">
-          <div className="flex justify-center">
-            {reference.imageUrl ? ( 
-              <div className="relative">
-                <Image src={reference.imageUrl} alt={reference.title} width={250} height={250} className="justify-self-center" /> 
-                <button
-                  className="absolute top-[-10px] right-[-10px] p-1 bg-red-500 text-white hover:bg-red-700 rounded-full"
-                  onClick={handleImageDelete}
-                >
-                  <X size={18} />
-                </button>
-              </div>
-            ) : ( 
-              <UploadDropzone 
-                endpoint="imageUploader"
-                onClientUploadComplete={(res) => {
-                  handleImageUpload(res.at(0).url);
-                }}
-                onUploadError={(error: Error) => {
-                console.log(error);
-                alert(`ERROR! ${error.message}`);
-                }}
-              /> 
-            )}
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <div  className="text-right">
-              Title
-            </div>
-            <input
-              id="title"
-              value={reference.title}
-              onChange={handleTitleChange}
-              className="col-span-3 border border-gray-300 rounded-md"
-              required
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <div className="text-right">
-              Description
-            </div>
-            <input
-              id="description"
-              value={reference.description}
-              onChange={handleDescriptionChange}
-              className="col-span-3 border border-gray-300 rounded-md"
-              required
-            />
-          </div>
-        </div>
-        <DialogFooter>
-          <button type="submit" onClick={handleSubmit}>Save changes</button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-  )
 }
 
 const AddMediaForm: FC<Props> = ({
