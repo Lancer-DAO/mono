57e02bea75371684c75f0497f7754b2d1c1603fc
mattdegods <matthewcpfeifer@gmail.com>
2023-08-28 18:25:42 -0400

    Temp: add try-catch to login Found source of my error - user context is broken rn Other issue is that onboardingInfo industry type is wrong - expects discipline etc

--- app/server/api/routers/users/login.ts
+++ app/server/api/routers/users/login.ts
@@ -18,16 +18,20 @@ export const login = protectedProcedure.mutation(async ({ ctx }) => {
       return user;
     }
   } else {
-    let user = await queries.user.getByEmail(email);
-    // console.log("picture", user.picture);
-    if (!user.picture) {
-      await queries.user.updatePicture(user.id, picture);
-      user = await queries.user.getByEmail(email);
+    try {
+      let user = await queries.user.getByEmail(email);
+      // console.log("picture", user.picture);
+      // if (!user.picture && picture) {
+      //   await queries.user.updatePicture(user.id, picture);
+      //   user = await queries.user.getByEmail(email);
+      // }
+      // if (!user.name && nickname) {
+      //   await queries.user.updateName(user.id, nickname);
+      //   user = await queries.user.getByEmail(email);
+      // }
+      return user;
+    } catch (e) {
+      console.error("❌!@!", e);
     }
-    if (!user.name) {
-      await queries.user.updateName(user.id, nickname);
-      user = await queries.user.getByEmail(email);
-    }
-    return user;
   }
 });
