2a59076856282b3159aeca050bb8bf9a20e5d1b0
jacksturt <jack@sturt.io>
2023-09-01 11:15:55 -0600

    add server side props rerouting

--- app/pages/leaderboard/earners.tsx
+++ app/pages/leaderboard/earners.tsx
@@ -1,8 +1,55 @@
 import Head from "next/head";
-import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { LeaderboardCommits } from "@/components/leaderboard/CommitBoard";
 import { TopEarnersBoard } from "@/components/leaderboard/TopEarnersBoard";
-export const getServerSideProps = withPageAuthRequired();
+import { GetServerSidePropsContext } from "next";
+import { prisma } from "@/server/db";
+export async function getServerSideProps(
+  context: GetServerSidePropsContext<{ id: string; req; res }>
+) {
+  withPageAuthRequired();
+  const { req, res } = context;
+  const metadata = await getSession(req, res);
+  if (!metadata?.user) {
+    return {
+      redirect: {
+        destination: "/api/auth/login",
+        permanent: false,
+      },
+    };
+  }
+  const { email } = metadata.user;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+      isAdmin: true,
+      hasFinishedOnboarding: true,
+      hasBeenApproved: true,
+    },
+  });
+
+  if (!user.hasFinishedOnboarding) {
+    return {
+      redirect: {
+        destination: "/welcome",
+        permanent: false,
+      },
+    };
+  }
+  if (!user.isAdmin) {
+    return {
+      redirect: {
+        destination: "/leaderboard",
+        permanent: false,
+      },
+    };
+  }
+  return { props: {} };
+}
 
 export default function Home() {
   return (
--- app/pages/leaderboard/quests.tsx
+++ app/pages/leaderboard/quests.tsx
@@ -1,8 +1,55 @@
 import Head from "next/head";
-import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { LeaderboardCommits } from "@/components/leaderboard/CommitBoard";
 import { TopQuestUsersBoard } from "@/components/leaderboard/TopQuestUsersBoard";
-export const getServerSideProps = withPageAuthRequired();
+import { GetServerSidePropsContext } from "next";
+import { prisma } from "@/server/db";
+export async function getServerSideProps(
+  context: GetServerSidePropsContext<{ id: string; req; res }>
+) {
+  withPageAuthRequired();
+  const { req, res } = context;
+  const metadata = await getSession(req, res);
+  if (!metadata?.user) {
+    return {
+      redirect: {
+        destination: "/api/auth/login",
+        permanent: false,
+      },
+    };
+  }
+  const { email } = metadata.user;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+      isAdmin: true,
+      hasFinishedOnboarding: true,
+      hasBeenApproved: true,
+    },
+  });
+
+  if (!user.hasFinishedOnboarding) {
+    return {
+      redirect: {
+        destination: "/welcome",
+        permanent: false,
+      },
+    };
+  }
+  if (!user.hasBeenApproved) {
+    return {
+      redirect: {
+        destination: "/account",
+        permanent: false,
+      },
+    };
+  }
+  return { props: {} };
+}
 
 export default function Home() {
   return (
