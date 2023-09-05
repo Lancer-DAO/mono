c9edfe61c76ef2d2bfebcfa78c43cdb9c473e1c0
mattdegods <matthewcpfeifer@gmail.com>
2023-09-04 20:27:53 -0400

    Fix: resume modal

--- app/components/organisms/ResumeModal.tsx
+++ app/components/organisms/ResumeModal.tsx
@@ -1,4 +1,5 @@
 import { FC, Dispatch, SetStateAction } from "react";
+import Image from "next/image";
 import { Modal } from "@/components";
 import { BountyActionsButton } from "../bounties/Bounty/components";
 
@@ -8,25 +9,41 @@ interface Props {
 
 const ResumeModal: FC<Props> = ({ setShowModal }) => {
   return (
-    <Modal setShowModal={setShowModal} disableOutsideClick className="py-20">
-      <div className="w-full px-10">
-        <div className="w-full flex items-start justify-center gap-20">
-          <div className="w-full flex flex-col gap-5 max-w-[400px]">
-            <h1>Welcome to the Lancer Public Beta</h1>
-            <p>
-              We are accepting applications for Lancers at this time. Please
-              upload your resume to complete the application process. We will be
-              accepting talent on a rolling basis.
-            </p>
-            <p>Your information will not be shared.</p>
-          </div>
-          <BountyActionsButton
-            type="green"
-            text="Got it"
-            onClick={() => setShowModal(false)}
-          />
-        </div>
+    <Modal
+      setShowModal={setShowModal}
+      disableOutsideClick
+      className="py-20 relative"
+    >
+      <div className="w-full flex flex-col items-center justify-center gap-5 max-w-[400px] mx-auto">
+        <h1 className="text-center">Welcome to the Lancer Public Beta</h1>
+        <p className="text-center">
+          We are accepting applications for Lancers at this time. Please upload
+          your resume via the Account page to complete the application process.
+        </p>
+        <p className="text-center">
+          We will be accepting talent on a rolling basis. Your information will
+          not be shared.
+        </p>
+        <BountyActionsButton
+          type="green"
+          text="Got it"
+          onClick={() => setShowModal(false)}
+        />
       </div>
+      <Image
+        src="/assets/images/knight_right.png"
+        width={154}
+        height={144}
+        className="absolute bottom-0 right-0"
+        alt="knight"
+      />
+      <Image
+        src="/assets/images/knight_left.png"
+        width={154}
+        height={144}
+        className="absolute bottom-0 left-0"
+        alt="knight"
+      />
     </Modal>
   );
 };
