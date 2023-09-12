b0d4315e30b02aee4759b9b15f2bcf53b31c9128
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2023-09-12 16:37:14 +0000

    fix svg

--- app/components/create/components/AdditionalInfo/tags/index.tsx
+++ app/components/create/components/AdditionalInfo/tags/index.tsx
@@ -69,8 +69,8 @@ const Tags = ({
               className="w-5 h-5"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
               />
             </svg>
