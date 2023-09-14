4d21ad91a7e553ccec3bc69e1b5da1d7401ff5ab
jacksturt <jack@sturt.io>
2023-09-13 23:03:45 -0600

    delete comment

--- app/server/api/routers/bounties/updateIsPrivate.ts
+++ app/server/api/routers/bounties/updateIsPrivate.ts
@@ -5,19 +5,15 @@ import * as queries from "@/prisma/queries";
 import { HostedHooksClient } from "../../webhooks";
 
 export const updateBountyToPrivate = protectedProcedure
-    .input(
-        z.object({
-            bountyId: z.number(),
-            isPrivate: z.boolean(),
-        })
-    )
-    .mutation(async ({ input : { bountyId, isPrivate }, ctx }) => {
-        const bounty = await queries.bounty.updateIsPrivate(
-            bountyId,
-            isPrivate
-        );
-
-        // const escrow = await queries.escrow.updateAmount
-        const returnValue = await queries.bounty.get(bountyId, ctx.user.id);
-        HostedHooksClient.sendWebhook(returnValue, "bounty is now private")
+  .input(
+    z.object({
+      bountyId: z.number(),
+      isPrivate: z.boolean(),
     })
+  )
+  .mutation(async ({ input: { bountyId, isPrivate }, ctx }) => {
+    const bounty = await queries.bounty.updateIsPrivate(bountyId, isPrivate);
+
+    const returnValue = await queries.bounty.get(bountyId, ctx.user.id);
+    HostedHooksClient.sendWebhook(returnValue, "bounty is now private");
+  });
