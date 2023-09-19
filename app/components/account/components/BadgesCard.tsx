6bd96ded909700e9852fa0c7e407653625cfa150
mattdegods <matthewcpfeifer@gmail.com>
2023-09-19 17:58:13 -0400

    Fix: badge loading and tooltip

--- app/components/account/components/BadgesCard.tsx
+++ app/components/account/components/BadgesCard.tsx
@@ -1,12 +1,11 @@
 import { FC, useEffect, useState } from "react";
 import { BADGES_PROJECT_PARAMS } from "@/src/constants";
-import { useUserWallet } from "@/src/providers";
 import { BountyNFT } from "@/types";
 import { createUnderdogClient } from "@underdog-protocol/js";
 import dayjs from "dayjs";
 import Image from "next/image";
 import badgeList from "./badgesnfts.json";
-import { Tooltip } from "@/components";
+import { LoadingBar, Tooltip } from "@/components";
 import { useAccount } from "@/src/providers/accountProvider";
 import { api } from "@/src/utils";
 
@@ -19,6 +18,22 @@ type BadgeListItem = {
   role: string;
   id: number;
   ownerAddress: string;
+};
+
+const Badge = ({ badge }) => {
+  const [hovered, setHovered] = useState(false);
+
+  return (
+    <div
+      className={`relative group ${hovered ? "z-50" : "z-0"}`}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      key={badge.id}
+    >
+      <Image src={badge.image} alt={badge.name} width={50} height={50} />
+      <Tooltip text={`Quest ${badge.name}`} />
+    </div>
+  );
 };
 
 const badgesList = badgeList as BadgeListItem[];
@@ -37,6 +52,7 @@ export const BadgesCard: FC = () => {
   );
 
   const [badges, setBadges] = useState<BountyNFT[]>([]);
+  const [loading, setLoading] = useState(false);
 
   const getBadgeNFTSForWallet = async (wallet: string) => {
     const nfts = await underdogClient.getNfts({
@@ -69,6 +85,7 @@ export const BadgesCard: FC = () => {
   };
 
   const fetchBadgeNFTs = async () => {
+    setLoading(true);
     const addresses = wallets.map((wallet) => wallet.publicKey);
     const badges = [];
     const allNfts = addresses.map(async (address) => {
@@ -78,6 +95,7 @@ export const BadgesCard: FC = () => {
     await Promise.all(allNfts);
 
     setBadges(badges);
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -89,23 +107,19 @@ export const BadgesCard: FC = () => {
   return (
     <div className="w-full md:w-[460px] max-h-[320px] rounded-xl bg-bgLancerSecondary/[8%] p-6">
       <p className="font-bold text-2xl text-textGreen pb-2">Badges</p>
-      {badges?.length > 0 ? (
+      {loading ? (
+        <div className="w-full flex items-center justify-center">
+          <LoadingBar title={null} />
+        </div>
+      ) : null}
+      {badges?.length > 0 && !loading ? (
         <div className="grid grid-cols-6 gap-2">
           {badges.map((badge) => (
-            <div className="relative group tag-item z-30" key={badge.id}>
-              <Image
-                src={badge.image}
-                alt={badge.name}
-                width={50}
-                height={50}
-              />
-              <Tooltip text={`Quest ${badge.name}`} />
-            </div>
+            <Badge badge={badge} key={badge.id} />
           ))}
         </div>
-      ) : (
-        <div>No Badges Yet</div>
-      )}
+      ) : null}
+      {badges?.length === 0 && !loading ? <div>No Badges Yet</div> : null}
     </div>
   );
 };
