357aca7f5cfc1e780cc286f02c86a86a2b975eaa
mattdegods <matthewcpfeifer@gmail.com>
2023-09-21 13:23:39 -0400

    Add no-apply placeholder image

--- app/components/quests/Quest/components/LancerApplyView.tsx
+++ app/components/quests/Quest/components/LancerApplyView.tsx
@@ -1,7 +1,7 @@
-import { Dispatch, FC, SetStateAction, useEffect, useState } from "react";
+import { Dispatch, FC, SetStateAction, useState } from "react";
 import ActionsCardBanner from "./ActionsCardBanner";
 import { useBounty } from "@/src/providers/bountyProvider";
-import { ChatButton, ContributorInfo } from "@/components";
+import { ChatButton } from "@/components";
 import { useUserWallet } from "@/src/providers";
 import { smallClickAnimation } from "@/src/constants";
 import { motion } from "framer-motion";
@@ -17,6 +17,7 @@ import { PublicKey } from "@solana/web3.js";
 import { api, updateList } from "@/src/utils";
 import toast from "react-hot-toast";
 import { QuestActionView } from "./QuestActions";
+import Image from "next/image";
 
 interface Props {
   setCurrentActionView: Dispatch<SetStateAction<QuestActionView>>;
@@ -141,95 +142,119 @@ const LancerApplyView: FC<Props> = ({ setCurrentActionView }) => {
             description="This Quest has been canceled. You can apply to other Quests."
           />
         )}
-      <div className="w-full h-full p-6 flex items-center justify-between gap-5">
-        <div className="flex items-center gap-4">
-          <p className="text-neutral600 text">Portfolio</p>
-          <input
-            type="text"
-            className="text border border-neutral200 placeholder:text-neutral500/60 
-            bg-neutral100 text-neutral500 w-[190px] h-[34px] rounded-md px-3"
-            name={`link-portfolio`}
-            placeholder="Paste Link"
-            id={`link-portfolio`}
-            disabled={applicationDisabled}
-            value={applyData.portfolio}
-            onChange={(e) =>
-              setApplyData({ ...applyData, portfolio: e.target.value })
-            }
-          />
+      {applicationDisabled && !currentBounty.isCompleter ? (
+        <div className="w-full h-[400px] flex flex-col items-center justify-center">
+          <div className="w-32">
+            <Image
+              src="/assets/images/placeholder.png"
+              width={419}
+              height={351}
+              alt="placeholder"
+            />
+          </div>
+          <p className="text-neutral600 text">You cannot apply to this Quest</p>
         </div>
-        <div className="flex items-center gap-4">
-          <p className="text-neutral600 text">LinkedIn</p>
-          <input
-            type="text"
-            className="text border border-neutral200 placeholder:text-neutral500/60 
-            bg-neutral100 text-neutral500 w-[190px] h-[34px] rounded-md px-3"
-            name={`link-linkedin`}
-            placeholder="Paste Link"
-            id={`link-linkedin`}
-            disabled={applicationDisabled}
-            value={applyData.linkedin}
-            onChange={(e) =>
-              setApplyData({ ...applyData, linkedin: e.target.value })
-            }
-          />
-        </div>
-      </div>
-      <div className="h-[1px] w-full bg-neutral200" />
-      <div className="w-full px-6 py-4 flex flex-col gap-4">
-        <p className="text-neutral600 text">Who am I?</p>
-        <textarea
-          className="text border border-neutral200 placeholder:text-neutral500/80 resize-none h-[232px]
-          bg-neutral100 text-neutral500 w-full rounded-md px-3 p-2 disabled:opacity-60"
-          name={`about`}
-          placeholder="Tell us about yourself"
-          id={`about`}
-          disabled={applicationDisabled}
-          value={applyData.about}
-          onChange={(e) =>
-            setApplyData({ ...applyData, about: e.target.value })
-          }
-        />
-        {/* TODO: resume upload - link brought from user object, can delete, can reupload */}
-        <div className="flex items-center justify-end text text-neutral600">
-          <button
-            className="rounded-md bg-white border border-neutral200 flex items-center justify-center gap-2 h-8 px-2"
-            disabled={applicationDisabled}
-            onClick={() =>
-              window.open(currentUser.resume, "_blank", "noopener noreferrer")
-            }
-          >
-            <ImageIcon color="#A1B2AD" size={18} />
-            <p className="text-xs text-neutral400 truncate">resume.pdf</p>
-          </button>
-        </div>
-      </div>
-      <div className="h-[1px] w-full bg-neutral200" />
-      <div className="w-full px-6 py-4 flex flex-col gap-4">
-        <p className="text-neutral-600 text">Why am I a good fit?</p>
-        <textarea
-          className="text border border-neutral200 placeholder:text-neutral500/80 resize-none h-[232px]
-          bg-neutral100 text-neutral500 w-full rounded-md px-3 p-2 disabled:opacity-60"
-          name={`details`}
-          placeholder="Type your message here..."
-          id={`details`}
-          disabled={applicationDisabled}
-          value={applyData.details}
-          onChange={(e) =>
-            setApplyData({ ...applyData, details: e.target.value })
-          }
-        />
-      </div>
-      {!applicationDisabled && (
-        <div className="flex items-center justify-end px-6 py-4">
-          <motion.button
-            {...smallClickAnimation}
-            className="bg-primary200 text-white h-9 w-fit px-4 py-2 title-text rounded-md"
-            onClick={onClick}
-          >
-            Submit Application
-          </motion.button>
-        </div>
+      ) : (
+        <>
+          <div className="w-full h-full p-6 flex items-center justify-between gap-5">
+            <div className="flex items-center gap-4">
+              <p className="text-neutral600 text">Portfolio</p>
+              <input
+                type="text"
+                className="text border border-neutral200 placeholder:text-neutral500/60 
+                bg-neutral100 text-neutral500 w-[190px] h-[34px] rounded-md px-3"
+                name={`link-portfolio`}
+                placeholder="Paste Link"
+                id={`link-portfolio`}
+                disabled={applicationDisabled}
+                value={applyData.portfolio}
+                onChange={(e) =>
+                  setApplyData({ ...applyData, portfolio: e.target.value })
+                }
+              />
+            </div>
+            <div className="flex items-center gap-4">
+              <p className="text-neutral600 text">LinkedIn</p>
+              <input
+                type="text"
+                className="text border border-neutral200 placeholder:text-neutral500/60 
+                bg-neutral100 text-neutral500 w-[190px] h-[34px] rounded-md px-3"
+                name={`link-linkedin`}
+                placeholder="Paste Link"
+                id={`link-linkedin`}
+                disabled={applicationDisabled}
+                value={applyData.linkedin}
+                onChange={(e) =>
+                  setApplyData({ ...applyData, linkedin: e.target.value })
+                }
+              />
+            </div>
+          </div>
+
+          <div className="h-[1px] w-full bg-neutral200" />
+          <div className="w-full px-6 py-4 flex flex-col gap-4">
+            <p className="text-neutral600 text">Who am I?</p>
+            <textarea
+              className="text border border-neutral200 placeholder:text-neutral500/80 resize-none h-[232px]
+              bg-neutral100 text-neutral500 w-full rounded-md px-3 p-2 disabled:opacity-60"
+              name={`about`}
+              placeholder="Tell us about yourself"
+              id={`about`}
+              disabled={applicationDisabled}
+              value={applyData.about}
+              onChange={(e) =>
+                setApplyData({ ...applyData, about: e.target.value })
+              }
+            />
+            {/* TODO: resume upload - link brought from user object, can delete, can reupload */}
+            <div className="flex items-center justify-end text text-neutral600">
+              <button
+                className="rounded-md bg-white border border-neutral200 flex items-center justify-center gap-2 h-8 px-2"
+                onClick={() => {
+                  if (currentUser.resume) {
+                    window.open(
+                      currentUser.resume,
+                      "_blank",
+                      "noopener noreferrer"
+                    );
+                  } else {
+                    toast.error("No resume uploaded");
+                  }
+                }}
+              >
+                <ImageIcon color="#A1B2AD" size={18} />
+                <p className="text-xs text-neutral400 truncate">resume.pdf</p>
+              </button>
+            </div>
+          </div>
+          <div className="h-[1px] w-full bg-neutral200" />
+          <div className="w-full px-6 py-4 flex flex-col gap-4">
+            <p className="text-neutral-600 text">Why am I a good fit?</p>
+            <textarea
+              className="text border border-neutral200 placeholder:text-neutral500/80 resize-none h-[232px]
+              bg-neutral100 text-neutral500 w-full rounded-md px-3 p-2 disabled:opacity-60"
+              name={`details`}
+              placeholder="Type your message here..."
+              id={`details`}
+              disabled={applicationDisabled}
+              value={applyData.details}
+              onChange={(e) =>
+                setApplyData({ ...applyData, details: e.target.value })
+              }
+            />
+          </div>
+          {!applicationDisabled && (
+            <div className="flex items-center justify-end px-6 py-4">
+              <motion.button
+                {...smallClickAnimation}
+                className="bg-primary200 text-white h-9 w-fit px-4 py-2 title-text rounded-md"
+                onClick={onClick}
+              >
+                Submit Application
+              </motion.button>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
