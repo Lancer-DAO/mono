2a23565f8af8dd91e59b5f95646d485667a0d954
mattdegods <matthewcpfeifer@gmail.com>
2023-09-26 15:03:38 -0400

    Update placeholder img and remove bg banner

--- app/components/templates/DefaultLayout.tsx
+++ app/components/templates/DefaultLayout.tsx
@@ -31,35 +31,33 @@ const DefaultLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
 const MobilePlaceholder = () => {
   return (
     <div className="relative w-screen h-screen bg-neutral100">
-      <div className="relative w-full h-full bg-[url('/assets/images/placeholder_bg.png')] bg-contain">
-        <div className="h-full flex flex-col items-center justify-center z-30">
-          <Image
-            src="/assets/images/Lancer-Green-No-Background-p-1080.png"
-            width={200}
-            height={200}
-            alt="mobile-placeholder"
-          />
-          <h1>Welcome, Lancer.</h1>
-          <p className="text-center mt-2 px-10 text-sm">
-            Please view our freelance marketplace on a desktop for the best
-            experience. Mobile coming soon :&#41;
-          </p>
-        </div>
+      <div className="h-full flex flex-col items-center justify-center z-30">
         <Image
-          src="/assets/images/knight_left.png"
-          width={150}
-          height={150}
-          alt="knight left"
-          className="absolute bottom-0 left-0"
+          src="/assets/images/lancer_desktop.svg"
+          width={200}
+          height={200}
+          alt="mobile-placeholder"
         />
-        <Image
-          src="/assets/images/knight_right.png"
-          width={150}
-          height={150}
-          alt="knight right"
-          className="absolute bottom-0 right-0"
-        />
+        <h1>Welcome to Lancer.</h1>
+        <p className="text-center mt-2 px-10 text-sm">
+          Please view our freelance marketplace on a desktop for the best
+          experience. Mobile coming soon :&#41;
+        </p>
       </div>
+      <Image
+        src="/assets/images/knight_left.png"
+        width={150}
+        height={150}
+        alt="knight left"
+        className="absolute bottom-0 left-0"
+      />
+      <Image
+        src="/assets/images/knight_right.png"
+        width={150}
+        height={150}
+        alt="knight right"
+        className="absolute bottom-0 right-0"
+      />
     </div>
   );
 };
