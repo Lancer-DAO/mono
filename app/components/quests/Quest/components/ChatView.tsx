394b9d3258fc663e5f19ff872962b8c727cd9fd2
ssamkkim <ssamuel.kkim@gmail.com>
2023-09-28 21:35:27 -0700

    add viewing new update to chat

--- app/components/quests/Quest/components/ChatView.tsx
+++ app/components/quests/Quest/components/ChatView.tsx
@@ -1,11 +1,13 @@
 import { BountyUserType } from "@/prisma/queries/bounty";
 import { smallClickAnimation } from "@/src/constants";
 import { useBounty } from "@/src/providers/bountyProvider";
+import { api } from "@/src/utils";
 import { BountyState } from "@/types";
 import { motion } from "framer-motion";
 import { X } from "lucide-react";
 import { Dispatch, FC, SetStateAction } from "react";
 import ActionsCardBanner from "./ActionsCardBanner";
+import AlertCard from "./AlertCard";
 import { QuestActionView } from "./QuestActions";
 
 interface Props {
@@ -15,6 +17,11 @@ interface Props {
 
 const ChatView: FC<Props> = ({ selectedSubmitter, setCurrentActionView }) => {
   const { currentBounty } = useBounty();
+
+  const { data: update } = api.update.getNewUpdateByBounty.useQuery(
+    { id: currentBounty.id },
+    { enabled: !!currentBounty }
+  );
   return (
     <div className="flex flex-col">
       <ActionsCardBanner
@@ -50,6 +57,26 @@ const ChatView: FC<Props> = ({ selectedSubmitter, setCurrentActionView }) => {
           </motion.button>
         )}
       </ActionsCardBanner>
+      <div className="px-5 pt-5">
+        {(update && currentBounty.isCreator) && (
+          <AlertCard
+            type="positive"
+            title="Nice!"
+            description="You have received an update!"
+          >
+            <button className="bg-white px-4 py-2" onClick={() => setCurrentActionView(QuestActionView.ViewUpdate)}>
+              View Update
+            </button>
+          </AlertCard>
+        )} 
+        {(update && currentBounty.isApprovedSubmitter) && (
+          <AlertCard
+            type="positive"
+            title="Nice!"
+            description="Your update has been sent!"
+          />
+        )}
+      </div>
     </div>
   );
 };
