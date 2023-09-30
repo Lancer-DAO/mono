a6a0475e761209d8374bc6e4a6b562f16333148a
mattdegods <matthewcpfeifer@gmail.com>
2023-09-29 23:17:14 -0400

    Reduce industry color opacity for now

--- app/src/utils/colors.ts
+++ app/src/utils/colors.ts
@@ -1,14 +1,14 @@
 export const bountyIndustryColor = (industry: string) => {
   return {
-    "text-neutral600 bg-industryRed border-industryRedBorder": [
+    "text-neutral600 bg-industryRed/60 border-industryRedBorder/40": [
       "Content",
     ].includes(industry),
 
-    "text-neutral600 bg-industryGreen border-industryGreenBorder": [
+    "text-neutral600 bg-industryGreen/60 border-industryGreenBorder/40": [
       "Engineering",
     ].includes(industry),
 
-    "text-neutral600 bg-industryBlue border-industryBlueBorder": [
+    "text-neutral600 bg-industryBlue/60 border-industryBlueBorder/40": [
       "Design",
     ].includes(industry),
   };
