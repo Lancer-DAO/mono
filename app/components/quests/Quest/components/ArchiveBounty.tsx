02ff9e59605387a02fc954249d9e6a742e397ceb
mattdegods <matthewcpfeifer@gmail.com>
2023-10-03 19:43:49 -0400

    Cleanup

--- app/components/quests/Quest/components/ArchiveBounty.tsx
+++ app/components/quests/Quest/components/ArchiveBounty.tsx
@@ -1,14 +1,7 @@
-import { BOUNTY_ACTIONS_TUTORIAL_I_INITIAL_STATE } from "@/src/constants/tutorials";
+import { useState } from "react";
 import { useUserWallet } from "@/src/providers";
 import { useBounty } from "@/src/providers/bountyProvider";
-import { useTutorial } from "@/src/providers/tutorialProvider";
-import { useReferral } from "@/src/providers/referralProvider";
 import { api } from "@/src/utils/api";
-import { PublicKey } from "@solana/web3.js";
-import { BOUNTY_USER_RELATIONSHIP } from "@/types/";
-import { updateList } from "@/src/utils";
-import { QuestActionsButton } from ".";
-import { useState } from "react";
 import toast from "react-hot-toast";
 
 export const ArchiveBounty = () => {
