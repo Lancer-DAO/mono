fafe6f3e1245e898d8c8783dc3ac723f65498155
mattdegods <matthewcpfeifer@gmail.com>
2023-10-10 16:43:20 -0400

    Clean up 404 + 500 pages

--- app/pages/404.tsx
+++ app/pages/404.tsx
@@ -1,18 +1,7 @@
-import { ProfileCreated } from "@/components";
-import Head from "next/head";
-import { Account } from "@/components/account/Account";
-import {
-  getAccessToken,
-  getSession,
-  withPageAuthRequired,
-} from "@auth0/nextjs-auth0";
-import { GetServerSidePropsContext } from "next";
-import * as queries from "@/prisma/queries";
-
 export const NotFound = () => {
   return (
-    <div className="w-full h-full">
-      <div className="h-[390px]"></div>
+    <div className="w-full h-screen flex flex-col items-center justify-center">
+      <h1 className="text-7xl">404</h1>
       <div className="text-lg text-center">
         Uh-Oh, seems like you&apos;ve ventured into unknown territory.
       </div>
